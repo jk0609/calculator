416a76c6d1c80d99c54671db588040584b5b5cb7
Chitroopa Manikkavasagam and Jonathon Kang <chitroopa+jonathon@>
2017-03-20 09:29:35 -0700

    Add multiply, subtract and divide operators and updated result alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+var add = function(num1, num2) {
+  return num1 + num2;
 };
+var subtract = function(num1, num2) {
+  return num1 - num2;
+};
+var multiply = function(num1, num2) {
+  return num1 * num2;
+};
+var divide = function(num1, num2) {
+  return num1 / num2;
+};
+
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var addresult = add(number1, number2);
+var subresult = subtract(number1, number2);
+var multresult = multiply(number1, number2);
+var divresult = divide(number1, number2);
+alert('Add:'+addresult+' Subtract:'+subresult+' Multiply:'+multresult+' Divide:'+divresult);
